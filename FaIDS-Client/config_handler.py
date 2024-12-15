8e4bdff348590b59a0e1267cf39c3d64470aa290
Filip Horvatinović Rumić <rumixpumix@gmail.com>
2024-12-15 23:22:28 +0100

    FINAL

---
+++ FaIDS-Client/config_handler.py
@@ -0,0 +1,232 @@
+import json
+import os
+import re
+from logging import log
+from logging import traceback_func
+config_updated_bool = False
+
+# Logging function
+
+def apply_current_config(configuration):
+        try:
+            with open("config.json", "w") as config_file:
+                json.dump(configuration, config_file)
+                log("Config file saved.", 4)
+        except PermissionError as error:
+            log(f"ERROR-CH-ACC-00-01-01: Permission error: {error}", 4)
+            log("Unable to write config to file config.json", 1)
+            return False
+        except IOError as error:
+            log(f"ERROR-CH-ACC-00-01-02: I/O error: {error}", 4)
+            log("I/O Error while saving config", 1)
+            return False
+        except Exception as error:
+            log(f"ERROR-CH-ACC-00-01-03: Unknown error: {error}", 1)
+            log("Unknown error occured on config applying", 1)
+            return False
+
+def validate_config(configuration):
+    def is_valid_ip(ip_address):
+        try:
+            # Regex for valid IPv4 excluding any octet being 255
+            ipv4_pattern = r"^(?!255(\.|\b))(?!\d{1,3}\.255(\.|\b))(\d{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-4])\.(\d{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-4])\.(\d{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-4])\.(\d{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-4])$"
+            
+            # Check if it matches the IPv4 regex
+            if re.match(ipv4_pattern, ip_address):
+                return True  # Valid IPv4 address
+            return False
+        except Exception as error:
+            log(f"Validating IP failed: {error}", 4)
+            return False
+    def is_valid_port(port):
+        try:
+            if port >= 1 and port <= 65535:
+                return True
+            else:
+                return False
+        except Exception as error:
+            log(f"Validating port failed: {error}", 4)
+            return False
+    def is_valid_debug_mode(debug):
+        try:
+            if debug == True or debug == False or debug.lower() == "true" or debug.lower() == "false":
+                return True
+            else:
+                return False
+        except Exception as error:
+            log(f"Validating debug mode failed: {error}", 4)
+    try:
+        ip_address = configuration["server_ip_address"]
+        if not is_valid_ip(ip_address):
+            log("Invalid IP address. Please check the server_ip_address configuration.", 1)
+            return False
+        port = configuration["server_port"]
+        if not is_valid_port(port):
+            log("Invalid port. Port should be between 1 and 65535.", 1)
+            return False
+        debug = configuration["debug_mode"]
+        if not is_valid_debug_mode(debug):
+            log("Invalid debug mode value. It should be either 'True' or 'False'.", 1)
+            return False
+        return True
+    except KeyError as error:
+        # First there is a debug statement giving us the error code. Number 4 signifies that when this log gets printed there's a debug statement next to it.
+        log(f"ERROR-CH-VC-00-01-01: Error on validating config: {error}", 4)
+        # Then there's the user error, that gives a brief description of what might've happened. Number 1 signifies the "Error" statement.
+        log("Config file is not configured properly. Keys were tampered with.", 1)
+        
+    except TypeError as error:
+        # Debug log for the technical details of the TypeError
+        log(f"ERROR-CH-VC-00-01-02: Unexpected type encountered: {error}", 4)
+        # User-friendly message for TypeError
+        log("A type mismatch occurred while processing the data. Please check the data types in the configuration.", 1)
+        traceback_func()
+        
+    except ValueError as error:
+        # Debug log for the technical details of the ValueError
+        log(f"ERROR-CH-VC-00-01-03: Invalid value encountered: {error}", 4)
+        # User-friendly message for ValueError
+        log("An invalid value was encountered. Please verify the values in the configuration.", 1)
+        traceback_func()
+        
+    except AttributeError as error:
+        # Debug log for the technical details of the AttributeError
+        log(f"ERROR-CH-VC-00-01-04: Attribute not found: {error}", 4)
+        # User-friendly message for AttributeError
+        log("An attribute is missing or misconfigured in the configuration file. Please review the setup.", 1)
+        traceback_func()
+        
+    except IndexError as error:
+        # Debug log for the technical details of the IndexError
+        log(f"ERROR-CH-VC-00-01-05: Index out of range: {error}", 4)
+        # User-friendly message for IndexError
+        log("An index out of range error occurred. Please check array or list indexes in the configuration.", 1)
+        traceback_func()
+        
+    except Exception as error:
+        # Debug log for the technical details of the unknown error
+        log(f"ERROR-CH-VC-00-01-01: Unexpected error: {error}", 4)
+        # User-friendly message for any unexpected errors
+        log("An unexpected error occurred. Please check the configuration and try again.", 1)
+        traceback_func()
+# Apply configuration by reading the config file
+def get_configuration():
+    try:
+        with open("config.json", "r") as config_file:
+            configuration = json.load(config_file)
+            if validate_config(configuration):
+                return configuration
+            else:
+                log("Configuration wasn't validated", 1)
+                try:
+                    if input("Do you wish to reset the config? (Y/N): ").lower() == "y":
+                        return configuration_first_time_setup() 
+                    else:
+                        exit()
+                except:
+                    exit()
+    except FileNotFoundError:
+        log(f"ERROR-CH-AC-00-01-01: File not found: {error}", 4)
+        log("File not found when applying the config.", 1)
+        return False
+    except json.JSONDecodeError as error:
+        log(f"ERROR-CH-AC-00-01-02: JSON Error: {error}", 4)
+        log("JSON error occured on applying config.", 1)
+        return False
+    except Exception as error:
+        log(f"ERROR-CH-AC-00-01-03: Unexpected error: {error}", 4)
+        log("Unexpected error occured on applying config.", 1)
+        traceback_func()
+        return False
+
+# First time configuration setup
+def configuration_first_time_setup():
+    temp_config = {}
+    if config_updated_bool and os.path.exists("config.json"):
+        current_config = get_configuration()
+        if current_config:
+            temp_config.update(current_config)
+    
+    try:
+        # More configuration addition settings
+        if "server_ip_address" not in temp_config:
+            server_ip_address = input("Input bind IP: ")
+            temp_config["server_ip_address"] = server_ip_address
+        
+        if "server_port" not in temp_config:
+            server_port = int(input("Input server port: "))
+            temp_config["server_port"] = server_port
+
+        if "debug_mode" not in temp_config:
+            debug_mode = ""
+            while debug_mode.upper() != "T" and debug_mode.upper() != "F":
+                debug_mode = input("Debug mode (T/F): ")
+            if debug_mode.upper() == "T":
+                temp_config["debug_mode"] = True
+            else:
+                temp_config["debug_mode"] = False
+
+        if not validate_config(temp_config):
+            log("Config wasn't validated! Want to try again?")
+            try:
+                if input("Y/N:").lower() == "y":
+                    configuration_first_time_setup()
+                else:
+                    exit()
+            except:
+                exit()
+        
+        # Save the new configuration to the file
+        return temp_config
+    except (ValueError, Exception) as error:
+        log(f"ERROR-CH-CFTS-00-01-01: Unexpected error: {error}", 4)
+        log("Unexpected error on config input!", 1)
+        traceback_func()
+        return False
+
+# Main configuration handler
+def configuration_handler():
+    global config_updated_bool
+
+    if os.path.exists("config.json"):
+        if config_updated_bool:
+            log("New configuration options! ", 3)
+            configuration = configuration_first_time_setup()
+            if configuration:
+                log("Updated the config file.", 4)
+                # Update the script to reset the `config_updated_bool`
+                with open(__file__, "r+") as program_code:
+                    lines = program_code.readlines()
+                    for i, line in enumerate(lines):
+                        if "config_updated_bool =" in line:
+                            lines[i] = "config_updated_bool = False\n"
+                            break
+                    program_code.seek(0)
+                    program_code.writelines(lines)
+                    program_code.truncate()
+                apply_current_config(configuration)
+                return
+        
+        # Apply configuration
+        configuration = get_configuration()
+        if configuration:
+            log("Configuration applied successfully.", 3)
+            return configuration
+        else:
+            log(f"No configuration was returned by apply_configuration function. ", 1)
+    else:
+        log("config.json not found. Proceeding with initial setup.", 2)
+        configuration = configuration_first_time_setup()
+        apply_current_config(configuration)
+        return configuration
+
+# Run the configuration handler
+if __name__ == "__main__":
+    log("This script is usually run by the main program. Proceed only if you know what you're doing.", 2)
+    try:
+        if input("Y/N: ").lower() == "y":
+            configuration_handler()
+        else:
+            exit()
+    except Exception:
+        exit()
--- FaIDS-Client/main.py
+++ FaIDS-Client/main.py
@@ -1,10 +1,262 @@
-import logging
+import hashlib
+import os
+import json
+import socket
+import ssl
+import pickle
+
+user_verified = False
+token = ""
+configuration = {}
+
+def receive_file(socket):
+    try:
+        clear_console()
+
+        # Receive metadata
+        metadata = socket.recv(1024).decode()
+        if not metadata:
+            log("Failed to receive metadata.", 2)
+            return False
+        
+        filename, filesize = metadata.split('|')
+        filename = os.path.basename(filename)  # Prevent path traversal
+        filesize = int(filesize)
+
+        log(f"Receiving file: {filename}, ({filesize} bytes)", 3)
+
+        # Ensure the directory exists
+        os.makedirs("files/receive", exist_ok=True)
+
+        # Receive the file
+        filepath = os.path.join("files/receive", filename)
+        with open(filepath, "wb") as file:
+            received = 0
+            while received < filesize:
+                data = socket.recv(min(1024, filesize - received))
+                if not data:
+                    log("Connection lost during file transfer.", 1)
+                    return False
+                file.write(data)
+                received += len(data)
+                log(f"Received {received}/{filesize} bytes", 3)
+
+        log("File transfer complete.", 3)
+        return True
+
+    except Exception as e:
+        log(f"Error receiving file: {e}", 1)
+        return False
 
 
-def main():
+def send_file(socket, filepath):
+    try:
+        # Validate file
+        if not os.path.isfile(filepath):
+            log(f"File does not exist: {filepath}", 1)
+            return False
+
+        filename = os.path.basename(filepath)
+        filesize = os.path.getsize(filepath)
+
+        # Send metadata
+        metadata = f"{filename}|{filesize}"
+        socket.sendall(metadata.encode())
+        log(f"Sending file: {filename}, ({filesize} bytes)", 3)
+
+        # Send the file
+        with open(filepath, "rb") as file:
+            while (chunk := file.read(1024)):
+                socket.sendall(chunk)
+        
+        log("File sent successfully.", 3)
+        return True
+
+    except Exception as e:
+        log(f"Error sending file: {e}", 1)
+        return False
+
+def send_server_action(socket, action, sub_action, username=None):
+    data_to_send = {}
+    data_to_send["token"] = token
+    data_to_send["action"] = action
+    data_to_send["sub-action"] = sub_action
+    if not username == None:
+        data_to_send["username"] = username
+
+    try:
+        # Serialize the dictionary
+        serialized_data = pickle.dumps(data_to_send)
+        
+        # Send the length of the serialized data first to allow the server to know how much data to expect
+        socket.sendall(len(serialized_data).to_bytes(4, 'big'))  # Send size in 4 bytes
+        
+        # Send the serialized dictionary
+        socket.sendall(serialized_data)
+
+        if sub_action != 1:
+            return
+
+        data_length = int.from_bytes(socket.recv(4), 'big')
+
+        serialized_data = socket.recv(data_length)
+
+        received_list = pickle.loads(serialized_data)
+
+        print(received_list)
+
+        return received_list
+    except Exception as e:
+        log(f"Error sending data to server: Error: {e}", 1)
+
+def server_connection_establisher(username, password):
+    global token
+
+    server_ip = configuration["server_ip_address"]
+
+    server_port = configuration["server_port"]
+
+    context = ssl.create_default_context()
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_NONE
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_stream:
+        socket_stream.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        socket_stream.connect((server_ip, server_port))
+        with context.wrap_socket(socket_stream, server_hostname=server_ip) as ssl_client_connection:
+            ssl_client_connection.sendall(username.encode())
+            ssl_client_connection.sendall(password.encode())
+            token = ssl_client_connection.recv(1024)
+            log("Received token, proceeding with program", 4)
+            server_communication_handler_session(token, ssl_client_connection)
+
+def server_communication_handler_session(token, ssl_client_connection):
+    options = [None, "Upload File", "Receive File","Domain Request"]
     
+    while True:
+        clear_console()
 
+        # Display options menu
+        print("Select an option:")
+        index = 1
+        for option in options:
+            if option is not None:
+                print(f"{index}. {option}")
+                index += 1
+        
+        try:
+            # Get user input and process it
+            user_choice = int(input("Option: "))
+            if user_choice == 1: #MISC ACTIONS = 1 - REQ ACTIVE USERS, 2-ACTUAL FILE SENDING
+                # Case 1: Upload File
+                files = os.listdir("files/send")
+                if files:
+                    print("Available files to upload:")
+                    index = 1
+                    for file in files:
+                        print(f"{index}. {file}")
+                        index += 1
+
+                    try:
+                        file_to_send_index = int(input("Select file to send: "))
+                        if 1 <= file_to_send_index <= len(files):
+                            file_to_send = files[file_to_send_index - 1]
+                            print(f"Selected file: {file_to_send}")
+                            active_users = send_server_action(ssl_client_connection, 1, 1)
+                            index = 1
+                            for active_user in active_users:
+                                if active_user is not None:
+                                    print(f"{index}. {active_user}")
+                                    index += 1
+                            try:
+                                selected_user = int(input("Select: "))
+                                send_server_action(ssl_client_connection, 1, 3, active_users[selected_user - 1])
+                                send_file(ssl_client_connection, f"files/send/{file_to_send}")
+                            except Exception as e:
+                                print(e)
+                        else:
+                            print("Invalid file selection.")
+                    except ValueError:
+                        print("Invalid input, please enter a valid number.")
+                else:
+                    print("No files available to upload.")
+            elif user_choice == 2:
+                input("Press enter to start receiving a file...")
+                send_server_action(ssl_client_connection, 1, 2)
+                if receive_file(ssl_client_connection):
+                    input()
+                    continue
+            elif user_choice == 3:
+                # Case 2: Domain Request
+                input("Press Enter to continue with Domain Request...")  # Implement domain request logic here
+                
+            else:
+                print("Invalid option. Please select a valid option.")
+
+        except ValueError:
+            log("Incorrect option, please enter a valid number.", 1)        
+
+def hash_credentials(username, password):
+    credentials = f"{username}:{password}"
+    credentials_hash_object = hashlib.sha256(credentials.encode())  # Encode text to bytes
+    credentials_hash_hex = credentials_hash_object.hexdigest()
+    return credentials_hash_hex
+
+def get_hashed_credentials():
+    with open("credentials/user_credentials.json", "r") as credentials_file:
+        return json.load(credentials_file)
+
+def login(username, password):
+    if hash_credentials(username, password) == get_hashed_credentials():
+        return True
+    else:
+        return False
+
+def register(username, password):
+    #dodat password checks
+    if not os.path.exists("credentials/user_credentials.json"):
+        os.makedirs("credentials")
+        with open("credentials/user_credentials.json", "w") as credentials_file:
+            pass
+    with open("credentials/user_credentials.json", "w") as credentials_file:
+        hashed_credentials = hash_credentials(username, password)
+        json.dump(hashed_credentials, credentials_file)
+        return True
+        
+def main():
+    global user_verified
+    #username = input("Username: ")
+    #password = input("Password: ")
+    username = "admin"
+    password = "Pa$$w0rd"
+    if input("Registering (y/n):").lower() == "y":
+        if register(username, password):
+            if login(username, password):
+                user_verified = True
+                print("Registered in successfully!")
+                server_connection_establisher(username, password)
+            else:
+                print("Failed to register!")
+                main()
+        else:
+            print("Register unsuccessful")
+            main()
+    else:
+        if login(username, password):
+            user_verified = True
+            print("Logged in successfully!")
+            server_connection_establisher(username, password)
+        else:
+            print("Failed to login.")
+            main()
+
+from logging import log
+from logging import clear_console
+from config_handler import configuration_handler
 
 if __name__ == "__main__":
-    try:
-    except Exception:
+    configuration = configuration_handler()
+    if configuration:
+        main()
+    else:
+        log("Failed!")
